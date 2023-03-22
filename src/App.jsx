f474074570ed65bb1910a5702b61b158cc4291a6
이경호 <65242726+kyoungholee@users.noreply.github.com>
2023-03-23 00:15:25 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import "./App.css";
 function App() {
   const onSubmit = (e) => {
     e.preventDefault();
+                   //맵함수 안에 데이터를 넣어서 formData에 넣는다.
     const formData = new FormData(e.target);
     // [['search', 'data']]
+    //formData를 다시 객체로 만드는 작업 
     const data = Object.fromEntries(formData.entries());
+    //값 추출
     alert(data.search);
     // debugger
   };
